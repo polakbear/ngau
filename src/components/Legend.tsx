ed8290db85015915739224e226e36f63843e1df1
sylwia malinowska <sylwia@polakbear.dev>
2025-05-18 11:22:02 +0200

    Refactor Legend component; update styles for improved layout and remove unused state

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -1,10 +1,8 @@
-import { useState, useMemo } from 'react';
+import { useMemo } from 'react';
 import { rankBasedColorScale } from '../utils/color';
 import styles from './Legend.module.css';
 
 export function Legend() {
-  const [showMethodology, setShowMethodology] = useState(false);
-
   const gradientStyle = useMemo(() => {
     const MAX_RANK = 194;
     const colorStops: string[] = [];
@@ -23,29 +21,23 @@ export function Legend() {
   }, []);
 
   return (
-    <>
-      <div className={styles.legend}>
-        <div className={styles.container}>
-          <div className={styles.scale}>
-            <div className={styles.bar}>
-              <div className={styles.noData}></div>
-              <div className={styles.gradient} style={gradientStyle}></div>
-            </div>
-            <div className={styles.row}>
-              <div className={styles.labels}>
-                <div className={styles.labelNoData}>No data</div>
-                <div className={styles.gradientLabels}>
-                  <span className={styles.labelVeryPoor}>Very Poor</span>
-                  <span className={styles.labelPoor}>Poor</span>
-                  <span className={styles.labelFair}>Fair</span>
-                  <span className={styles.labelGood}>Good</span>
-                  <span className={styles.labelExcellent}>Excellent</span>
-                </div>
-              </div>
-            </div>
+    <div className={styles.legend}>
+      <div className={styles.container}>
+        <div className={styles.noData}></div>
+        <div className={styles.scale}>
+          <div className={styles.bar}>
+            <div className={styles.gradient} style={gradientStyle}></div>
           </div>
         </div>
+        <div className={styles.labelNoData}>No data</div>{' '}
+        <div className={styles.gradientLabels}>
+          <span className={styles.labelVeryPoor}>Very Poor</span>
+          <span className={styles.labelPoor}>Poor</span>
+          <span className={styles.labelFair}>Fair</span>
+          <span className={styles.labelGood}>Good</span>
+          <span className={styles.labelExcellent}>Excellent</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
