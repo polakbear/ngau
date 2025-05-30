8915354fff0596dbfdd17c8bcfcd7452fc344def
sylwia malinowska <sylwia@polakbear.dev>
2025-05-30 20:02:30 +0200

    refactor: migrate GeoData context to separate provider and hook for improved data management (#24)

--- src/contexts/GeoDataContext.tsx
+++ src/contexts/GeoDataContext.tsx
@@ -1,14 +1,7 @@
-import React, {
-  createContext,
-  useContext,
-  useMemo,
-  useEffect,
-  useState,
-} from 'react';
-import { geoCentroid } from 'd3-geo';
+import { createContext } from 'react';
 import type { CountryData } from '../types';
 
-interface GeoDataContextValue {
+export interface GeoDataContextValue {
   geoJson: any;
   data: CountryData[];
 }
@@ -17,44 +10,4 @@ const GeoDataContext = createContext<GeoDataContextValue | undefined>(
   undefined
 );
 
-export const GeoDataProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  const [geoJson, setGeoJson] = useState<any>(null);
-  const [data, setData] = useState<CountryData[]>([]);
-
-  useEffect(() => {
-    let isMounted = true;
-    fetch('/countries.geojson')
-      .then((res) => res.json())
-      .then((geo) => {
-        geo.features.forEach((f: any) => {
-          f.__centroid = geoCentroid(f);
-        });
-        if (isMounted) {
-          setGeoJson(geo);
-        }
-      });
-    fetch('/data.json')
-      .then((res) => res.json())
-      .then((d) => {
-        if (isMounted) setData(d);
-      });
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
-  const value = useMemo(() => ({ geoJson, data }), [geoJson, data]);
-  return (
-    <GeoDataContext.Provider value={value}>{children}</GeoDataContext.Provider>
-  );
-};
-
-export function useGeoDataContext(): GeoDataContextValue {
-  const context = useContext(GeoDataContext);
-  if (!context) {
-    throw new Error('useGeoDataContext must be used within a GeoDataProvider');
-  }
-  return context;
-}
+export default GeoDataContext;
