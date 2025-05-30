d3f478dc464ee294b6e770ad4b28c96b92cda0e8
sylwia malinowska <sylwia@polakbear.dev>
2025-05-30 15:59:41 +0200

    refactor: move searchReducer into SearchProvider for better encapsulation

--- src/contexts/SearchProvider.tsx
+++ src/contexts/SearchProvider.tsx
@@ -1,6 +1,5 @@
 import { useReducer, ReactNode } from 'react';
 import { SearchContext, SearchState, SearchAction } from './SearchContext';
-import { searchReducer } from '../components/Search/reducer';
 
 const initialState: SearchState = {
   searchQuery: '',
@@ -8,6 +7,41 @@ const initialState: SearchState = {
   suggestions: [],
   selectedIndex: -1,
 };
+
+function searchReducer(
+  state: SearchState,
+  action: SearchAction,
+  initialState: SearchState
+): SearchState {
+  switch (action.type) {
+    case 'SET_QUERY':
+      return {
+        ...state,
+        searchQuery: action.query,
+        suggestions: action.suggestions,
+        selectedIndex: -1,
+      };
+    case 'SET_EXPANDED':
+      return {
+        ...state,
+        isExpanded: action.expanded,
+      };
+    case 'SET_SUGGESTIONS':
+      return {
+        ...state,
+        suggestions: action.suggestions,
+      };
+    case 'SET_SELECTED_INDEX':
+      return {
+        ...state,
+        selectedIndex: action.index,
+      };
+    case 'RESET':
+      return { ...initialState };
+    default:
+      return state;
+  }
+}
 
 export default function SearchProvider({ children }: { children: ReactNode }) {
   const [state, dispatch] = useReducer(
